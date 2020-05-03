cb602791e867d53f3569a1fd291ba890a1ea9bd1
Fransiskusandika <dikakoko@icloud.com>
2020-05-03 23:02:09 +0700

    Updates

---
+++ precache-manifest.e2ad234dac70a7edbd8cd5747418bb51.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d74886d6ab7fd3c005c48860f9fe80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2677c013ffd39f733c20",
+    "url": "/static/css/main.6a018319.chunk.css"
+  },
+  {
+    "revision": "1434c4dfd8dac45d6345",
+    "url": "/static/js/2.6e68c49c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.6e68c49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2677c013ffd39f733c20",
+    "url": "/static/js/main.5fd1af0e.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "021afd896e3a0119702aab318a58472a",
+    "url": "/static/media/1.021afd89.jpg"
+  },
+  {
+    "revision": "021afd896e3a0119702aab318a58472a",
+    "url": "/static/media/2.021afd89.jpg"
+  },
+  {
+    "revision": "021afd896e3a0119702aab318a58472a",
+    "url": "/static/media/3.021afd89.jpg"
+  },
+  {
+    "revision": "6cfef2367721acc155d1e04320ca28c9",
+    "url": "/static/media/about-bg.6cfef236.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/static/media/header-bg.e2d2a51c.jpg"
+  }
+]);
